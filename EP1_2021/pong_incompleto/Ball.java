4c29fb550ef3fad34d54026c65936e3d96009b80
Jeniffer Martins <martins.jeniffer@usp.br>
2021-06-24 23:03:53 -0300

    FEAT: Identação e melhorias no código

--- EP1_2021/pong_incompleto/Ball.java
+++ EP1_2021/pong_incompleto/Ball.java
@@ -6,233 +6,197 @@ import java.awt.*;
 */
 
 public class Ball {
+  private double cx;
+  private double cy;
+  private double width;
+  private double height;
+  private Color color;
+  private double speed;
+  private double directionX;
+  private double directionY;
+  private Player playerColisao;
 
-	/**
-		Construtor da classe Ball. Observe que quem invoca o construtor desta classe define a velocidade da bola 
-		(em pixels por millisegundo), mas não define a direção deste movimento. A direção do movimento é determinada 
-		aleatóriamente pelo construtor.
+  /**
+    Construtor da classe Ball. Observe que quem invoca o construtor desta classe define a velocidade da bola 
+    (em pixels por millisegundo), mas não define a direção deste movimento. A direção do movimento é determinada 
+    aleatóriamente pelo construtor.
 
-		@param cx coordenada x da posição inicial da bola (centro do retangulo que a representa).
-		@param cy coordenada y da posição inicial da bola (centro do retangulo que a representa).
-		@param width largura do retangulo que representa a bola.
-		@param height altura do retangulo que representa a bola.
-		@param color cor da bola.
-		@param speed velocidade da bola (em pixels por millisegundo).
-	*/
+    @param cx coordenada x da posição inicial da bola (centro do retangulo que a representa).
+    @param cy coordenada y da posição inicial da bola (centro do retangulo que a representa).
+    @param width largura do retangulo que representa a bola.
+    @param height altura do retangulo que representa a bola.
+    @param color cor da bola.
+    @param speed velocidade da bola (em pixels por millisegundo).
+  */
 
-	double cx;
-	double cy;
-	double width;
-	double height;
-	Color color;
-	double speed;
-	double speedX;
-	double speedY;
-	Player playerColisao;
+  public Ball(
+    double cx,
+    double cy,
+    double width,
+    double height,
+    Color color,
+    double speed
+  ) {
+    this.cx = cx;
+    this.cy = cy;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.speed = speed;
+    this.directionX = (this.speed);
+    this.directionY = (this.speed);
+  }
 
-	public Ball(double cx, double cy, double width, double height, Color color, double speed){
-		this.cx = cx;
-		this.cy = cy;
-		this.width = width;
-		this.height = height;
-		this.color = color;
-		this.speed = speed;
-		this.speedX = (this.speed);
-		this.speedY = (this.speed);
-	}
-
-	/**
+  /**
 		Método chamado sempre que a bola precisa ser (re)desenhada.
 		Detalhe: o y começa a ser contado de cima para baixo
 		O meio da arena é 400 e 300
 		a bola encosta na borda inferior direita quando está na posição 770 e 570
 	*/
 
-	public void draw(){
-		GameLib.setColor(color);
-		GameLib.fillRect(cx, cy, width, height);
-	}
+  public void draw() {
+    GameLib.setColor(color);
+    GameLib.fillRect(cx, cy, width, height);
+  }
 
-	/**
+  /**
 		Método chamado quando o estado (posição) da bola precisa ser atualizado.
 		
 		@param delta quantidade de millisegundos que se passou entre o ciclo anterior de atualização do jogo e o atual.
 	*/
 
-	public void update(long delta){
-		cx += speedX * delta;
-		cy += speedY * delta;
-	}
+  public void update(long delta) {
+    cx += this.directionX * delta;
+    cy += this.directionY * delta;
+  }
 
-	/**
+  /**
 		Método chamado quando detecta-se uma colisão da bola com um jogador.
 	
 		@param playerId uma string cujo conteúdo identifica um dos jogadores.
 	*/
 
-	public void onPlayerCollision(String playerId){
-		// if (playerId.equals("Player 1")) {
-		// 	if(playerColisao.getCx() + playerColisao.getWidth()/2.0 + 5 >= cx - width/2.0 &&	// frente
-		//       (playerColisao.getHeight()/2.0 + playerColisao.getCy() >= (cy + height/2.0) && 
-		//        playerColisao.getCy() - playerColisao.getHeight()/2.0  <= (cy - height/2.0))){
-		// 		this.speedX = this.speedX*(-1);
-		// 	}
-		// 	else if( cy + height/2.0 == playerColisao.getCy() - playerColisao.getHeight() && 		// em cima
-		// 			(cx + width/2.0 >= playerColisao.getCx() - playerColisao.getWidth()/2.0 && cx + width/2.0 <= playerColisao.getCx() + playerColisao.getWidth()/2.0 )){
-		// 		this.speedY = this.speedY*(-1);
-		// 	}
-		// 	else if( cy - height/2.0 == playerColisao.getCy() + playerColisao.getHeight() && 		// em baixo
-		// 			(cx + width/2.0 >= playerColisao.getCx() - playerColisao.getWidth()/2.0 && cx + width/2.0 <= playerColisao.getCx() + playerColisao.getWidth()/2.0 )){
-		// 		this.speedY = this.speedY*(-1);
-		// 	}
-		// 	else if(cx + width/2.0 == playerColisao.getCx() - playerColisao.getWidth() &&		// atrás
-		// 			(cy - height/2.0 >= playerColisao.getCy() - playerColisao.getHeight()/2.0 && cy - height/2.0 <= playerColisao.getCy() + playerColisao.getHeight()/2.0 ) ){
-		// 		this.speedX = this.speedX*(-1);
-		// 	}
-		// } else if(playerId.equals("Player 2")) {
-		// 	if (playerColisao.getCx() - playerColisao.getWidth()/2.0 <= cx + width/2.0 && // em frente
-		// 		(playerColisao.getHeight()/2.0 + playerColisao.getCy() - (cy + height/2.0) >= 0 && 
-		// 		playerColisao.getCy() - playerColisao.getHeight()/2.0  <= (cy - height/2.0))){
-		// 		this.speedX = this.speedX*(-1);
-		// 	}
-		// 	else if(cy + height/2.0 == playerColisao.getCy() - playerColisao.getHeight() && 		// em cima
-		// 			(cx + width/2.0 >= playerColisao.getCx() - playerColisao.getWidth()/2.0 && cx + width/2.0 <= playerColisao.getCx() + playerColisao.getWidth()/2.0 )){
-		// 		this.speedY = this.speedY*(-1);
-		// 	}
-		// 	else if(cy - height/2.0 == playerColisao.getCy() + playerColisao.getHeight() && 		// em baixo
-		// 			(cx + width/2.0 >= playerColisao.getCx() - playerColisao.getWidth()/2.0 && cx + width/2.0 <= playerColisao.getCx() + playerColisao.getWidth()/2.0 )){
-		// 		this.speedY = this.speedY*(-1);
-		// 	}
-		// 	else if(cx - width/2.0 == playerColisao.getCx() + playerColisao.getWidth() &&		// atrás
-		// 			( cy - height/2.0 >= playerColisao.getCy() - playerColisao.getHeight()/2.0 && cy - height/2.0 <= playerColisao.getCy() + playerColisao.getHeight()/2.0 ) ){
-		// 		this.speedX = this.speedX*(-1);
-		// 	}
-		// }
-	}
+  public void onPlayerCollision(String playerId) {
+    if (playerId.equals("Player 1")) {
+      this.directionX = Math.abs(this.speed);
+    } else {
+      this.directionX = -Math.abs(this.speed);
+    }
+  }
 
-	/**
+  /**
 		Método chamado quando detecta-se uma colisão da bola com uma parede.
 		@param wallId uma string cujo conteúdo identifica uma das paredes da quadra. |
 	*/
 
-	public void onWallCollision(String wallId){
-		if((cx >= 30 && cx <= 770) && (cy <= 120)){  // cima 
-			this.speedY = this.speedY*(-1);
-		}
-		else if((cx >= 30 && cx <= 770) && (cy >= 570)){  // baixo
-			this.speedY = this.speedY*(-1);
-		}
-		else if((cy >= 30 && cy <= 570) && (cx >= 30 || cx <= 770)){  // direita e esquerda
-			this.speedX = this.speedX*(-1);
-		}
-		else if((cx <= 30 && cy <= 120 ) || (cx <= 30 && cy >= 570) || (cx >= 770 && cy >= 570) || (cx >= 770 && cy >= 570) ){  // áreas de bug
-			this.speedX = this.speedX*(-1);
-			this.speedY = this.speedY*(-1);
-		}
-		System.out.println(wallId);
-	}
+  public void onWallCollision(String wallId) {
+    if (wallId.equals("Top")) {
+      this.directionY = Math.abs(this.speed);
+    } else if (wallId.equals("Bottom")) {
+      this.directionY = -Math.abs(this.speed);
+    } else if (wallId.equals("Left")) {
+      this.directionX = Math.abs(this.speed);
+    } else if (wallId.equals("Right")) {
+      this.directionX = -Math.abs(this.speed);
+    }
+  }
 
-	/**
+  /**
 		Método que verifica se houve colisão da bola com uma parede.
 		@param wall referência para uma instância de Wall contra a qual será verificada a ocorrência de colisão da bola.
 		@return um valor booleano que indica a ocorrência (true) ou não (false) de colisão.
 	*/
-	
-	public boolean checkCollision(Wall wall){
-		// if((wall.getId().equals("Left")) && (wall.getCx() >= this.cx)){
-		// 	return true;
-		// }
-		// if((wall.getId().equals("Right")) && (wall.getCx() <= this.cx)){
-		// 	return true;
-		// }
-		// if((wall.getId().equals("Bottom")) && (wall.getCy() <= this.cy)){
-		// 	return true;
-		// }
-		// if((wall.getId().equals("Top")) && (wall.getCy() >= this.cy)){
-		// 	return true;
-		// }
-		// if((cx <= 30 && cy <= 120 ) || (cx <= 30 && cy >= 570) || (cx >= 770 && cy >= 570) || (cx >= 770 && cy >= 570) ){
-		// 	return true;
-		// }
 
-		return false;
-	}
+  public boolean checkCollision(Wall wall) {
+    String wallId = wall.getId();
 
-	/**
+    if (wallId.equals("Top")) {
+      double ballTop = this.cy - (this.height / 2);
+      double wallBottom = wall.getCy() + (wall.getHeight() / 2);
+
+      if (ballTop <= wallBottom) {
+        return true;
+      }
+    } else if (wallId.equals("Bottom")) {
+      double ballBottom = this.cy + (this.height / 2);
+      double wallTop = wall.getCy() - (wall.getHeight() / 2);
+
+      if (ballBottom >= wallTop) {
+        return true;
+      }
+    } else if (wallId.equals("Left")) {
+      double ballLeft = this.cx - (this.height / 2);
+      double wallRight = wall.getCx() + (wall.getWidth() / 2);
+
+      if (ballLeft <= wallRight) {
+        return true;
+      }
+    } else if (wallId.equals("Right")) {
+      double ballRight = this.cx + (this.width / 2);
+      double wallLeft = wall.getCx() - (wall.getWidth() / 2);
+
+      if (ballRight >= wallLeft) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  /**
 		Método que verifica se houve colisão da bola com um jogador.
 		@param player referência para uma instância de Player contra o qual será verificada a ocorrência de colisão da bola.
 		@return um valor booleano que indica a ocorrência (true) ou não (false) de colisão.
-	*/	
+	*/
 
-	public boolean checkCollision(Player player){
-		// if(player.getId().equals("Player 1")) {
-		// 	playerColisao = player;
-		// 	if(player.getCx() + player.getWidth()/2.0 + 5 >= cx - width/2.0  &&	// frente
-		//       (player.getHeight()/2.0 + player.getCy() >= (cy + height/2.0) && 
-		//        playerColisao.getCy() - playerColisao.getHeight()/2.0  <= (cy - height/2.0))){
-		// 		return true;
-		// 	}
-		// 	else if( cy + height/2.0 == player.getCy() - player.getHeight() && 		// em cima
-		// 			(cx + width/2.0 >= player.getCx() - player.getWidth()/2.0 && cx + width/2.0 <= player.getCx() + player.getWidth()/2.0 )){
-		// 		return true;
-		// 	}
-		// 	else if( cy - height/2.0 == player.getCy() + player.getHeight() && 		// em baixo
-		// 			(cx + width/2.0 >= player.getCx() - player.getWidth()/2.0 && cx + width/2.0 <= player.getCx() + player.getWidth()/2.0 )){
-		// 		return true;
-		// 	}
-		// 	else if( cx + width/2.0 == player.getCx() - player.getWidth() &&		// atrás
-		// 			(cy - height/2.0 >= player.getCy() - player.getHeight()/2.0 && cy - height/2.0 <= player.getCy() + player.getHeight()/2.0 ) ){
-		// 		return true;
-		// 	}
-		// }
-		// else if(player.getId().equals("Player 2")) {
-		// 	playerColisao = player;
-		// 	if (player.getCx() - player.getWidth()/2.0 <= cx + width/2.0 && // em frente
-		// 		(player.getHeight()/2.0 + player.getCy() - (cy + height/2.0) >= 0 && 
-		// 		playerColisao.getCy() - playerColisao.getHeight()/2.0  <= (cy - height/2.0))){
-		// 		return true;
-		// 	}
-		// 	else if(cy + height/2.0 == player.getCy() - player.getHeight() && 		// em cima
-		// 			(cx + width/2.0 >= player.getCx() - player.getWidth()/2.0 && cx + width/2.0 <= player.getCx() + player.getWidth()/2.0 )){
-		// 		return true;
-		// 	}
-		// 	else if(cy - height/2.0 == player.getCy() + player.getHeight() && 		// em baixo
-		// 			(cx + width/2.0 >= player.getCx() - player.getWidth()/2.0 && cx + width/2.0 <= player.getCx() + player.getWidth()/2.0 )){
-		// 		return true;
-		// 	}
-		// 	else if(cx - width/2.0 == player.getCx() + player.getWidth() &&		// atrás
-		// 			(cy - height/2.0 >= player.getCy() - player.getHeight()/2.0 && cy - height/2.0 <= player.getCy() + player.getHeight()/2.0 ) ){
-		// 		return true;
-		// 	}
-		// }
-		return false;
-	}
+  public boolean checkCollision(Player player) {
+    double ballTop = this.cy - (this.height / 2);
+    double ballBottom = this.cy + (this.height / 2);
 
-	/**
+    double ballLeft = this.cx - (this.width / 2);
+    double ballRight = this.cx + (this.width / 2);
+
+    // hitbox do player
+    double playerTop = player.getCy() - (player.getHeight() / 2);
+    double playerBottom = player.getCy() + (player.getHeight() / 2);
+
+    double playerLeft = player.getCx() - (player.getWidth() / 2);
+    double playerRight = player.getCx() + (player.getWidth() / 2);
+
+    // colisoes
+    boolean rightColision = ballLeft <= playerRight;
+    boolean leftColision = ballRight >= playerLeft;
+
+    boolean isBallBetweenPlayerTopAndBottom =
+      ballBottom >= playerTop && ballTop <= playerBottom;
+
+    return rightColision && leftColision && isBallBetweenPlayerTopAndBottom;
+  }
+
+  /**
 		Método que devolve a coordenada x do centro do retângulo que representa a bola.
 		@return o valor double da coordenada x.
 	*/
-	
-	public double getCx(){
-		return cx;
-	}
 
-	/**
+  public double getCx() {
+    return cx;
+  }
+
+  /**
 		Método que devolve a coordenada y do centro do retângulo que representa a bola.
 		@return o valor double da coordenada y.
 	*/
 
-	public double getCy(){
-		return cy;
-	}
+  public double getCy() {
+    return cy;
+  }
 
-	/**
+  /**
 		Método que devolve a velocidade da bola.
 		@return o valor double da velocidade.
 	*/
 
-	public double getSpeed(){
-		return speed;
-	}
-
+  public double getSpeed() {
+    return speed;
+  }
 }
--- EP1_2021/pong_incompleto/Score.java
+++ EP1_2021/pong_incompleto/Score.java
@@ -8,50 +8,48 @@ import java.awt.*;
 
 public class Score {
 
-	/**
-		Construtor da classe Score.
+  /**
+    Construtor da classe Score.
 
-		@param playerId uma string que identifica o player ao qual este placar está associado.
-	*/
-	String playerId;
-	int score;
+    @param playerId uma string que identifica o player ao qual este placar está associado.
+  */
+  String playerId;
+  int score;
 
-	public Score(String playerId){
-		this.score = 0;
-		this.playerId = playerId;
-	}
+  public Score(String playerId) {
+    this.score = 0;
+    this.playerId = playerId;
+  }
 
-	/**
+  /**
 		Método de desenho do placar.
 	*/
 
-	public void draw(){
-		if (this.playerId.equalsIgnoreCase("Player 1")){
-			GameLib.setColor(Color.GREEN);
-			GameLib.drawText(playerId + ": " + this.score, 70, GameLib.ALIGN_LEFT);
-		}
-		else {
-			GameLib.setColor(Color.BLUE);
-			GameLib.drawText(playerId + ": " + this.score, 70, GameLib.ALIGN_RIGHT);
-		}
-	}
+  public void draw() {
+    if (this.playerId.equalsIgnoreCase("Player 1")) {
+      GameLib.setColor(Color.GREEN);
+      GameLib.drawText(playerId + ": " + this.score, 70, GameLib.ALIGN_LEFT);
+    } else {
+      GameLib.setColor(Color.BLUE);
+      GameLib.drawText(playerId + ": " + this.score, 70, GameLib.ALIGN_RIGHT);
+    }
+  }
 
-	/**
+  /**
 		Método que incrementa em 1 unidade a contagem de pontos.
 	*/
 
-	public void inc(){
-		this.score++;
-	}
-		
+  public void inc() {
+    this.score++;
+  }
 
-	/**
+  /**
 		Método que devolve a contagem de pontos mantida pelo placar.
 
 		@return o valor inteiro referente ao total de pontos.
 	*/
 
-	public int getScore(){
-		return this.score;
-	}
+  public int getScore() {
+    return this.score;
+  }
 }
