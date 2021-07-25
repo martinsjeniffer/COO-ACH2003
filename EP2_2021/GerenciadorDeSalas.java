e7d98cde4b4ec5b7ca73f19e0268c832f1bf4a67
Jeniffer Martins <39566330+martinsjeniffer@users.noreply.github.com>
2021-07-25 23:08:27 +0000

    FEAT: comentarios e testes na main

--- EP2_2021/GerenciadorDeSalas.java
+++ EP2_2021/GerenciadorDeSalas.java
@@ -2,34 +2,44 @@ import java.time.LocalDateTime;
 import java.util.*;
 
 public class GerenciadorDeSalas {
+  /* que deve receber o nome da sala, a capacidade máxima da sala, e uma descrição;*/
   public void adicionaSalaChamada(String nome, int capacidadeMaxima, String descricao) {
 
   }
-
+  
+  /*que deve receber  o nome da sala a ser removida;*/
   public void removeSalaChamada(String nomeDaSala) {
 
   }
 
+  /*que deve devolver uma instância de List com objetos do tipo Sala; */
   public List<Sala> listaDeSalas() {
     return null;
   }
 
+  /*que deve receber uma instância de Sala;*/
   public void adicionaSala(Sala novaSala) {
 
   }
 
+  /* que recebe um nome de sala, um LocalDateTime que indica o início da reserva e um outro LocalDateTime para indicar o final da reserva. O método deve devolver uma instância de Reserva;*/
   public Reserva reservaSalaChamada(String nomeDaSala, LocalDateTime dataInicial, LocalDateTime dataFinal) {
     return null;
   }
 
+  
+  /*que recebe um objeto do tipo Reserva e cancela esta reserva;*/
   public void cancelaReserva(Reserva cancelada) {
 
   }
 
+  /*que devolve uma Collection de objetos Reserva que representam as reservas da 
+ respectiva sala.*/
   public Collection<Reserva> reservasParaSala(String nomeSala) {
     return null;
   }
-
+  
+  /*que recebe uma String com o nome da sala e imprime todas as suas reservas */
   public void imprimeReservasDaSala(String nomeSala) {
 
   }
--- EP2_2021/MarcadorDeReuniao.java
+++ EP2_2021/MarcadorDeReuniao.java
@@ -3,9 +3,27 @@ import java.time.*;
 
 public class MarcadorDeReuniao{
   public static void main(String[] args) {
+    Collection<String> listaDeParticipantes = new ArrayList<String>();
 
+    String emailTeste = "janedoe@gmail.com";
+    String emailTeste2 = "janedoe2222@gmail.com";
+
+    LocalDateTime inicio = LocalDateTime.of(2021,07,25,15,10,05);
+    LocalDateTime fim = LocalDateTime.of(2021,07,25,16,10,05);
+
+    listaDeParticipantes.add(emailTeste);
+    listaDeParticipantes.add(emailTeste2);
+
+    Participante janeDoe = new Participante(emailTeste, inicio, fim);
+    System.out.println(janeDoe.getEmail() + " " + janeDoe.getInicio() + " " + janeDoe.getFim());
+
+    System.out.println("Participantes: " + listaDeParticipantes);
+    
+    // indicaDisponibilidadeDe("janedoe@gmail.com", LocalDateTime.of(2018,07,23,15,10,05), LocalDateTime.of(2018,07,23,16,10,05));
   }
 
+
+  /*onde cada participante é identificado com uma String e o início e fim da disponibilidade é indicado com dias e horários dados por instâncias de java.time.LocalDateTime.*/
   public void indicaDisponibilidadeDe(String participante, LocalDateTime inicio, LocalDateTime fim){
 
   }
