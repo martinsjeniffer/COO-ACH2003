58ba6d672345e4a0a4fc6c377f0c3693a2b468f7
Jeniffer Martins <39566330+martinsjeniffer@users.noreply.github.com>
2021-07-30 23:32:36 +0000

    FIX: funcionando indicaDisponibilidade e criacao de uma classe main

--- EP2_2021/MarcadorDeReuniao.java
+++ EP2_2021/MarcadorDeReuniao.java
@@ -4,45 +4,25 @@ import java.time.*;
 public class MarcadorDeReuniao{
   Reuniao reuniao;
 
-  public static void main(String[] args) {
-    Collection<String> listaDeParticipantes = new ArrayList<String>();
-
-    String emailTeste = "janedoe@gmail.com";
-    String emailTeste2 = "janedoe2222@gmail.com";
-
-    LocalDateTime inicio = LocalDateTime.of(2021,07,25,15,10,05);
-    LocalDateTime fim = LocalDateTime.of(2021,07,25,16,10,05);
-
-    listaDeParticipantes.add(emailTeste);
-    listaDeParticipantes.add(emailTeste2);
-
-    Participante janeDoe = new Participante(emailTeste, inicio, fim);
-    System.out.println(janeDoe.getEmail() + " " + janeDoe.getInicio() + " " + janeDoe.getFim());
-
-    System.out.println("Participantes: " + listaDeParticipantes);
-
-
-    String nomeSala = "reuniao teste";
-    int capacidadeMaxima = 10;
-    String descricaoSala = "descricao de teste";
-    
-    // indicaDisponibilidadeDe("janedoe@gmail.com", LocalDateTime.of(2018,07,23,15,10,05), LocalDateTime.of(2018,07,23,16,10,05));
+  public MarcadorDeReuniao(Reuniao reuniao) {
+    this.reuniao = reuniao;
   }
-
 
   /*onde cada participante é identificado com uma String e o início e fim da
    de java.time.LocalDateTime.*/
   public void indicaDisponibilidadeDe(String participante, LocalDateTime inicio, LocalDateTime fim){
     /*compara o inicio e o fim da disponibilidade um participante (recbido no argumento) com o inicio e o fim de uma reunião */
+    System.out.println("aaaa");
 
     /* verificando a legibilidade reuniao nos horarios passados como argumentos*/
     if(inicio.isAfter(fim)){
       System.out.println("O inicio deve ser antes do final");
       return;
     }
+    System.out.println("bbbb " + this.reuniao.getInicioIntervalo().atStartOfDay() + this.reuniao.getFinalIntervalo());
        
-    LocalDateTime inicioReuniao= reuniao.getInicioIntervalo().atStartOfDay();    
-    LocalDateTime fimReuniao = reuniao.getFinalIntervalo().atTime(23,59,59);
+    LocalDateTime inicioReuniao= this.reuniao.getInicioIntervalo().atStartOfDay();    
+    LocalDateTime fimReuniao = this.reuniao.getFinalIntervalo().atTime(23,59,59);
 
     if(!((inicio.isAfter(inicioReuniao) || inicio.isEqual(inicioReuniao))
     && (fim.isBefore(fimReuniao) || fim.isEqual(fimReuniao)))){
@@ -50,9 +30,11 @@ public class MarcadorDeReuniao{
       return;
     }
 
+    System.out.println("cccc");
     ArrayList<Participante> Participantes = reuniao.getParticipantes();
     Participante participanteInformado = null;
 
+    System.out.println("cccc");
     //procurando o participante recebido no argumento pelo email na lista de Participantes da reunião
     for(Participante part : Participantes){
       if(part.getEmail().equals(participante)){
@@ -61,6 +43,7 @@ public class MarcadorDeReuniao{
         break;
       }
     }
+    System.out.println("444444");
 
     if(participanteInformado != null){
       //Participante existe -- Inclui um intervalo disponível dele
